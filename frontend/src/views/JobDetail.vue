477bf2dd3c743825f1ce9006baf3685487b61ed0
DanielGrgic <61809976+DanielGrgic@users.noreply.github.com>
2020-05-17 22:11:57 +0200

    Add files via upload

---
+++ frontend/src/views/JobDetail.vue
@@ -0,0 +1,52 @@
+<template>
+    <div v-if="job" class="JobDetail">
+        
+        <proba :proba="job"   />
+    </div>
+</template>
+
+<script>
+
+import proba from '@/components/proba.vue';
+import {Posts} from '@/services';
+
+export default {
+    props: ['id'],
+    data() {
+        return {
+            job: {
+              id: "5eb400586bceea42ac98ebf5",
+              ime:"Marko",
+              prezime:"Markić",
+              opis_posla:"I need the autocad expert for my job. Details will be shared.",
+              kategorija_posla:"Engineering and science",
+              naziv_posla:"AutoCAD expert",
+              potrebne_vjestine:"CAD/CAM, Solidworks, AutoCAD, Drafting, Building architecture...",
+              zarada:"50$",
+              posted_at:1588854872335
+             }
+        };
+    },
+    //async mounted() {
+    //    console.log(this.id);
+        //upit na backend
+    //    this.job = await Posts.getOne(this.id);
+    //    console.log(this.job)
+    //},
+
+    name: 'Job-Detail',
+    components: {
+        
+        proba
+    }
+};
+</script>
+
+<style scoped>
+button {
+    margin-bottom: 20px;
+}
+.JobDetail{
+    width: 100%;
+}
+</style>
---
+++ frontend/src/views/Jobs.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="pozadina">
+      <div class="naslov">
+          <div @click="gotoDetails(job)" :key="job.id" v-for="job in jobs">
+            <OneJob :info="job" />
+          </div>   
+      </div>
+    
+  </div>    
+</template>
+
+<script>
+import _ from 'lodash';
+import { Posts } from '@/services';
+import OneJob from '@/components/OneJob.vue';
+import proba from '@/components/proba.vue';
+
+
+export default {
+  data() {
+        return {
+           jobs: [
+             {
+              id: "5eb400586bceea42ac98ebf5",
+              ime:"Marko",
+              prezime:"Markić",
+              opis_posla:"I need the autocad expert for my job. Details will be shared.",
+              kategorija_posla:"Engineering and science",
+              naziv_posla:"AutoCAD expert",
+              potrebne_vjestine:"CAD/CAM, Solidworks, AutoCAD, Drafting, Building architecture...",
+              zarada:"50$",
+              posted_at:1588854872335
+             },
+             {
+              id: "5eb401a33cc232358ca86c94",
+              ime:"Ivan",
+              prezime:"Ivanić",
+              opis_posla:"I need the HTML expert for my job. Details will be shared.",
+              kategorija_posla:"WEB designing",
+              naziv_posla:"HTML expert",
+              potrebne_vjestine:"HTML, CSS, PHP, WEB DESIGNING",
+              zarada:"100$",
+              posted_at:1588854871335
+             }
+            ],
+            
+
+        };
+    },
+    //watch: {
+    //    'store.searchTerm': _.debounce(function(val) {
+    //        this.fetchPosts(val);
+    //    }, 500)
+    //},
+   // created() {
+    //    this.GetAllJobs();
+        
+    //},
+    name: 'Jobs',
+    methods: {
+        //async GetAllJobs() {
+
+        //    this.jobs = await Posts.getAll();
+          //  console.log(this.jobs)
+
+        //},
+        gotoDetails(job) {
+            this.$router.push({ path: `Job/${job.id}` });
+        }
+        //gotoDetails(card) {
+        //    this.$router.push({ path: `post/${card.id}` });
+        //}
+    },
+    components: {
+        OneJob,
+        proba
+    }
+  
+}
+</script>
+
+<style scoped>
+.pozadina{
+    background-color:white;
+    width: 70%;
+    margin: 5% auto;
+    
+  }
+
+  .naslov{
+    
+    width: 90%;
+    text-align: left;
+    margin: auto;
+    margin-bottom: 5%;
+    padding: 2% 0;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    background-color:white;
+  }
+  
+
+</style>  
